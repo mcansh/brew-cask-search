8bf61d98fe991258fe4cb27294c1b4071b429eff
Logan McAnsh <logan@mcan.sh>
2020-11-08 21:31:39 -0500

    feat: add sentry
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/entry-browser.tsx
+++ app/entry-browser.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Remix from '@remix-run/react/browser';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 import { App } from './app';
+
+Sentry.init({
+  dsn: `https://7d9da9ccdcc34dc29d33a54da6d28e2a@o74198.ingest.sentry.io/5509044`,
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV,
+  integrations: [new Integrations.BrowserTracing()],
+});
 
 ReactDOM.hydrate(
   // @ts-expect-error @types/react-dom says the 2nd argument to ReactDOM.hydrate() must be a
--- app/entry-server.tsx
+++ app/entry-server.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import type { EntryContext } from '@remix-run/core';
 import Remix from '@remix-run/react/server';
+import * as Sentry from '@sentry/node';
 
 import { App } from './app';
+
+Sentry.init({
+  dsn: `https://7d9da9ccdcc34dc29d33a54da6d28e2a@o74198.ingest.sentry.io/5509044`,
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV,
+});
 
 export default function handleRequest(
   request: Request,
